6f1f2d8902c5276f2f44ba0ad7e7f9cde29b5daf
chinezan <chinezan@yandex.ru>
2017-02-14 00:32:39 +0400

    добавлен класс Client

--- app.rb
+++ app.rb
@@ -1,5 +1,12 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:123.db"
+
+class Client < ActiveRecord::Base
+end
 
 configure do
   enable :sessions
