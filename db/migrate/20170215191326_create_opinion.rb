1d273997e3803ef35b9d7468e0cb9c0e9d6733c1
slim1979 <chinezan@yandex.ru>
2017-02-15 22:18:02 +0300

    create_opinion init

---
+++ db/migrate/20170215191326_create_opinion.rb
@@ -0,0 +1,9 @@
+class CreateOpinion < ActiveRecord::Migration[5.0]
+  def change
+	create_table :opinions do |t|
+			t.text :client_name
+			t.text :client_email
+			t.text :opinion_text
+		end
+  end
+end
